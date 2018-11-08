2dc0d75657a536201b6aff91b212758a8563dd3a
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-08 22:46:29 +0000

    Debe existir la cantidad de grasa, la prueba falla

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,12 +1,17 @@
 require 'etiqueta'
 
 RSpec.describe Etiqueta do
+
   it "has a version number" do
     expect(Etiqueta::VERSION).not_to be nil
   end
  
   it "Existe un nombre para la etiqueta" do
     p=Informacion_nutricional.new("producto")
+  end
+
+  it "Existe la cantidad de grasas" do
+	p=Informacion_nutricional.new("producto",3.2)
   end
  
 end
