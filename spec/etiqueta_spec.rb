d032b49c7c3cd03a5447e23e2b43f1ea59ac595c
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-08 21:43:11 +0000

    Comprobar que existe la clase informacion_nutricional, la prueba falla

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,8 +1,11 @@
-require 'lib/etiqueta/etiqueta'
+require "lib/etiqueta/etiqueta"
 
 RSpec.describe Etiqueta do
   it "has a version number" do
     expect(Etiqueta::VERSION).not_to be nil
+  end 
+  it "Comprobar que existe la clase etiqueta" do
+    p=Etiqueta.new
   end
 
 end
