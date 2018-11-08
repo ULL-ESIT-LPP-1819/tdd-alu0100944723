c38570b778a062b22dc413fd07f33df1a5c3c40a
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-08 23:09:56 +0000

    Existen las grasas saturadas, monoinsaturadas y poliinsaturadas, la prueba falla

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,16 +1,16 @@
 require 'etiqueta'
 
 RSpec.describe Etiqueta do
-  it "has a version number" do
-    expect(Etiqueta::VERSION).not_to be nil
-  end
- 
-  it "Existe un nombre para la etiqueta" do
-    p=Informacion_nutricional.new("producto",3.2)
-  end
+	
+	it "has a version number" do
+		expect(Etiqueta::VERSION).not_to be nil
+	end
 
-  it "Existe la cantidad de grasas" do
-	p=Informacion_nutricional.new("producto",3.2)
-  end
+describe "Comprobando el constructor de la etiqueta" do 
+
+	it "Existe un nombre para la etiqueta y la cantidad de grasas saturadas,monoinsaturadas y poliinsaturadas" do
+		p=Informacion_nutricional.new("producto",1.2,1.4,0.2)
+	end
+end
  
 end
