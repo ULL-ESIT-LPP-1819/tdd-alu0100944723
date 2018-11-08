b7de72da6f4bb7d8d645a03acc38cdad7b8cfbf7
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-08 21:25:47 +0000

    Tiene que existir un archivo etiqueta.rb, la prueba falla

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,9 +1,8 @@
+require 'lib/etiqueta/etiqueta'
+
 RSpec.describe Etiqueta do
   it "has a version number" do
     expect(Etiqueta::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
