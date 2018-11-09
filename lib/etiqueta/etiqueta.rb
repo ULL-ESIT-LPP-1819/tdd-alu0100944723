a8d44bd5c28d18d57970f3f73c07d63859867900
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-09 00:26:32 +0000

    Se almacena correctamente el nombre de la etiqueta, la prueba funciona

--- lib/etiqueta/etiqueta.rb
+++ lib/etiqueta/etiqueta.rb
@@ -1,6 +1,9 @@
 class Informacion_nutricional
+	
+	attr_reader :nombre
 
 	def initialize (nombre,grasasat,grasam,grasap,hidratos,azucares,proteinas,sal)
 		@nombre,@grasasat,@grasam,@grasap,@hidratos,@azucares,@proteinas,@sal=nombre,grasasat,grasam,grasap,hidratos,azucares,proteinas,sal
 	end		
 end
+
