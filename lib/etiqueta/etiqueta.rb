77825c156e0f8c9057b9543f2a15910fd257ff38
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-09 00:55:16 +0000

    Se almacena correctamente la sal, la prueba funciona

--- lib/etiqueta/etiqueta.rb
+++ lib/etiqueta/etiqueta.rb
@@ -1,6 +1,6 @@
 class Informacion_nutricional
 	
-	attr_reader :nombre, :grasasat, :grasam, :grasap, :hidratos, :azucares, :proteinas
+	attr_reader :nombre, :grasasat, :grasam, :grasap, :hidratos, :azucares, :proteinas, :sal
 
 	def initialize (nombre,grasasat,grasam,grasap,hidratos,azucares,proteinas,sal)
 		@nombre,@grasasat,@grasam,@grasap,@hidratos,@azucares,@proteinas,@sal=nombre,grasasat,grasam,grasap,hidratos,azucares,proteinas,sal
