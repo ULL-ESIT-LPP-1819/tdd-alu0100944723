52fd02095838ba614a6a1930fcf6e8d0123c7c7b
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-09 00:22:00 +0000

    Comprobando el acceso al nombre, la prueba falla

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -2,15 +2,26 @@ require 'etiqueta'
 
 RSpec.describe Etiqueta do
 	
+	before :each do
+		@e=Informacion_nutricional.new("producto",1.2,1.4,0.2,6.7,6.7,2.6,0.18)
+	end
+
 	it "has a version number" do
 		expect(Etiqueta::VERSION).not_to be nil
 	end
 
 describe "Comprobando el constructor de la etiqueta" do 
 
-	it "Existe un nombre para la etiqueta y la cantidad de grasas saturadas,monoinsaturadas y poliinsaturadas. Existen la cantidad de hidratos de carbono, azucares,proteinas y sal" do
+	it "Existe un nombre para la etiqueta y la cantidad de grasas saturadas,monoinsaturadas y poliinsaturadas.Existen la cantidad de hidratos de carbono, azucares,proteinas y sal" do
 		p=Informacion_nutricional.new("producto",1.2,1.4,0.2,6.7,6.7,2.6,0.18)
 	end
 end
- 
+
+describe "Comprobando los accesos" do
+	it "Se almacena correctamente el nombre de la etiqueta" do
+		expect(@e.nombre).to eq("producto")
+	end
 end
+
+end
+
