a3d5b39211c34fc64c9115580cae704554704e4c
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-11-22 19:39:25 +0000

    Existe la clase individuo, la prueba funciona

--- lib/etiqueta.rb
+++ lib/etiqueta.rb
@@ -3,6 +3,8 @@
 require "etiqueta/version"
 require "etiqueta/etiqueta"
 require "etiqueta/lista" 
+require "etiqueta/individuo"
+
 
 module Etiqueta
   # Your code goes here...
---
+++ lib/etiqueta/individuo.rb
@@ -0,0 +1,3 @@
+class Individuo 
+end
+
