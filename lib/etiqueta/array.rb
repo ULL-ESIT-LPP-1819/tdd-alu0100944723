4f4eead40ad97cf812ea0f4bedf5ebcac9c22f91
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2018-12-20 23:14:42 +0000

    Añadiendo el ordenar_for a la clase array, la prueba funciona

--- lib/etiqueta/array.rb
+++ lib/etiqueta/array.rb
@@ -1,6 +1,18 @@
 class  Array
-	def suma	
+	def suma
 		self.collect{|x| x.collect {|x| x.collect{|x| x.valorenergeticokcal.round(2)}}.reduce("+").reduce("+").round(3)}
 	end
-
+	
+	def ordenar_for
+		for i in 0..((self.count)-1)
+			for j in i+1..self.count-1
+				if self.suma[i] > self.suma[j]
+					aux=self[i]
+					self[i]=self[j]
+					self[j]=aux
+				end
+			end
+		end	
+		self			 
+	end
 end
